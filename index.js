4f558f649825177138092a4dac051cd685081c32
Albina Mukhamadeeva <albina_ips@list.ru>
2023-02-14 21:14:05 +0500

    added answers to questions in the Readme file; added index.html, index.js, index.css

---
+++ index.js
@@ -0,0 +1,9 @@
+let image = document.getElementById("elem-img");
+
+function clickNext() {
+    image.src = './assets/images/cat2.jpg';
+}
+
+function clickPrevious() {
+    image.src = './assets/images/cat1.jpg';
+}
