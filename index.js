f1255b54d57ae3ac28f5400ccaf878ee7aab9b9c
Albina Mukhamadeeva <albina_ips@list.ru>
2023-02-14 21:27:37 +0500

    changed the index.js file (added the solution of task No. 1)

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function clickNext() {
 function clickPrevious() {
     image.src = './assets/images/cat1.jpg';
 }
+
+//Number 1
+function showMessage() {
+    console.log('Я учу JavaScript!');
+}
+showMessage();
